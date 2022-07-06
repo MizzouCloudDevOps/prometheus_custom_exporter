a5bf1286e77f333e358aca61cbbe7f1c39cd6480
wangso <wangso@missouri.edu>
2022-07-06 14:15:08 -0500

    Update rand_num.py

--- rand_num.py
+++ rand_num.py
@@ -9,21 +9,22 @@ class RandomNumberCollector(object):
     def __init__(self):
         pass
     def collect(self):
-        gauge = GaugeMetricFamily("random_number", "A random number generator, I have no better idea", labels=["randomNum"])
-        gauge.add_metric(['random_num'], random.randint(1, 20))
+        rand_num = random.randint(1,20)
+        gauge = GaugeMetricFamily("random_number", "A random number gauge", labels=["randomNum"])
+        gauge.add_metric(['random_num'], rand_num)
         yield gauge
-        count = CounterMetricFamily("random_number_2", "A random number 2.0", labels=['randomNum'])
+        count = CounterMetricFamily("random_number_2", "A random number counter", labels=['randomNum'])
         global totalRandomNumber
-        totalRandomNumber += random.randint(1,30)
+        totalRandomNumber += rand_num
         count.add_metric(['random_num'], totalRandomNumber)
         yield count
 
 if __name__ == "__main__":
     port = 9001
     frequency = 1
-    
+
     start_http_server(port)
     REGISTRY.register(RandomNumberCollector())
-    while True: 
+    while True:
         # period between collection
         time.sleep(frequency)
